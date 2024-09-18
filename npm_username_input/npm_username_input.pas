04daacfa81b39767f5787690a69be3442168a0ad
Pateeer <72560259+PateeerQ@users.noreply.github.com>
2024-09-18 10:19:29 +0700

    Update npm_username_input.pas

--- npm_username_input/npm_username_input.pas
+++ npm_username_input/npm_username_input.pas
@@ -10,7 +10,7 @@
 
 (* Headers *)
 
-program npm_username;
+program input_test;
 uses crt;
 
 (* Variables *)
@@ -19,33 +19,54 @@ Var
     npm     : string;
     kelas   : string;
     nama    : string;
+    nilai_absen   : integer;
+    nilai_tugas   : integer;
+    nilai_uts     : integer;
+    nilai_uas     : integer;
+    nilai_akhir   : real;
 
 (* Main Function *)
 
 begin
     clrscr; // Clear Screen
-    
+
     Writeln ('__________________________________');
-    Writeln ('    Latihan (input) 12/09/2024    ');
+    Writeln ('    Latihan (input) 18/09/2024    ');
     Writeln ('__________________________________');
     Writeln ('');
 
-    Write ('NPM: ');
+    Write ('Masukkan NPM Anda: ');
     Readln (npm);
 
-    Write ('Nama: ');
+    Write ('Masukkan Nama Anda: ');
     Readln (nama);
-    
-    Write ('Kelas: ');
+
+    Write ('Masukkan Kelas Anda: ');
     Readln (kelas);
-    Writeln ('');
+    
+    Write ('Masukkan Nilai Absen Anda: ');
+    Readln (nilai_absen);
+    
+    Write ('Masukkan Nilai Tugas Anda: ');
+    Readln (nilai_tugas);
+    
+    Write ('Masukkan Nilai UTS Anda: ');
+    Readln (nilai_uts);
+    
+    Write ('Masukkan Nilai UAS Anda: ');
+    Readln (nilai_uas);
+    
+    clrscr;
 
     Writeln ('__________________________________');
-    
+
     Writeln ('NPM Anda: ', npm);
     Writeln ('Nama Anda: ', nama);
     Writeln ('Kelas Anda: ', kelas);
+    
+    nilai_akhir := (nilai_absen * 0.1) + (nilai_tugas * 0.2) + (nilai_uts * 0.3) + (nilai_uas * 0.4);
 
+    Writeln ('Nilai Akhir anda adalah: ', nilai_akhir);
     Writeln ('__________________________________');
 
     Readln;
