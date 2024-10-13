48c4a88dec7fcce5a156bb214fbfd7c2cb3697e1
Pateeer Q <fathirchannel@gmail.com>
2024-10-13 12:49:44 +0700

    better nilai_to_alpha

--- nilai_to_alpha/nilai_to_alpha.pas
+++ nilai_to_alpha/nilai_to_alpha.pas
@@ -35,25 +35,15 @@ begin
     Writeln ('__________________________________');
 
     if (nilai >= 0) then
-      begin
-        alpha := 'E';
-        if (nilai >= 40) then
-          begin
-            alpha := 'D';
-            if (nilai >= 60) then
-              begin
-                alpha := 'C'; 
-                if (nilai >= 70) then
-                  begin
-                    alpha := 'B';   
-                    if (nilai >= 90) then
-                      begin
-                        alpha := 'A';
-                      end;
-                  end;
-              end;
-          end;
-      end;
+      alpha := 'E'
+    else if (nilai >= 40) then
+      alpha := 'D'
+    else if (nilai >= 60) then
+      alpha := 'C'
+    else if (nilai >= 70) then
+      alpha := 'B'
+    else if (nilai >= 90) then
+      alpha := 'A';
 
     Writeln ('Nilai anda: ', nilai, ' Golongan: ', alpha);
 
